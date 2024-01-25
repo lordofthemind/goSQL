c63aa91ab93d425852e8787825c0c88677eedcb4
lordofthemind <supermanishkesh98@gmail.com>
2024-01-25 11:18:17 +0530

    Initial commit

---
+++ insert_data.sql
@@ -0,0 +1,30 @@
+INSERT INTO album
+  (title, artist, price)
+VALUES
+  ('Kind of Blue', 'Miles Davis', 45.50),
+  ('A Love Supreme', 'John Coltrane', 59.75),
+  ('Take Five', 'Dave Brubeck', 27.45),
+  ('My Favorite Things', 'John Coltrane', 50.25),
+  -- Additional Rows
+  ('Some Album', 'Some Artist', 19.99),
+  ('Another Album', 'Another Artist', 29.99),
+  ('Random Jazz', 'Random Artist', 22.75),
+  ('Cool Tunes', 'Cool Artist', 18.50),
+  ('Jazzy Vibes', 'Jazzy Artist', 30.00),
+  ('Smooth Jazz', 'Smooth Artist', 26.80),
+  ('Funky Beats', 'Funky Artist', 35.20),
+  ('Melodic Sounds', 'Melodic Artist', 40.15),
+  ('Groovy Jazz', 'Groovy Artist', 28.90),
+  ('Harmony', 'Harmony Artist', 33.25),
+  ('Rhythmic Patterns', 'Rhythmic Artist', 23.40),
+  ('Saxophone Serenade', 'Saxophone Artist', 31.60),
+  ('Trumpet Triumph', 'Trumpet Artist', 37.75),
+  ('Bass Grooves', 'Bass Artist', 42.90),
+  ('Piano Pleasures', 'Piano Artist', 39.50),
+  ('Drum Beats', 'Drum Artist', 45.80),
+  ('Guitar Grooves', 'Guitar Artist', 36.25),
+  ('Vocal Harmony', 'Vocal Artist', 47.30),
+  ('Rhythm Section', 'Rhythm Artist', 32.90),
+  ('Instrumental Odyssey', 'Instrumental Artist', 49.99),
+  ('New Jazz', 'New Artist', 21.50),
+  ('Smooth Rhythms', 'Rhythmic Artist', 38.75);
